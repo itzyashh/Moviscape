c10909eed0a860f278e017947eaed8d5a305b238
itzyashh <yashjadhav1411@rocketmail.com>
2022-11-29 19:10:27 +0530

    feat: password hash and fix: duplicate email bug

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,14 @@
-exports.create = (req, res) => {
+const user = require("../models/user")
+const bcrypt = require("bcrypt")
+exports.create = async (req, res) => {
   const { name, email, password } = req.body
-  console.log(name)
-  res.status(200).json({ name: `${name}` })
+
+  const oldUser = await user.findOne({ email })
+  if (oldUser)
+    return res.status(401).json({ error: "Email already registered!" })
+  bcrypt.hash(password, 8, function (err, hash) {
+    const newUser = new user({ name, email, password: hash })
+    newUser.save()
+    return res.status(200).json({ success: { name, email, password: hash } })
+  })
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,6 @@ const express = require("express")
 const { create: createUser } = require("../controllers/user")
 const router = express.Router()
 
-router.get("/create", createUser)
+router.post("/create", createUser)
 
 module.exports = router
