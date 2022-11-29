2f4d337108a1bb78ac301a49a837d998dc5be3cd
itzyashh <yashjadhav1411@rocketmail.com>
2022-11-30 02:59:51 +0530

    feat: add utils
    Refactorization of code

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,26 +3,17 @@ const EmailVerificationToken = require("../models/emailVerificationToken")
 const bcrypt = require("bcrypt")
 const nodemailer = require("nodemailer")
 const { isValidObjectId } = require("mongoose")
+const { generateOTP, generateMailTransporter } = require("../utils/mail")
+const { sendError } = require("../utils/helper")
 
 exports.create = async (req, res) => {
   const { name, email, password } = req.body
 
   const oldUser = await User.findOne({ email })
-  if (oldUser)
-    return res.status(401).json({ error: "Email already registered!" })
+  if (oldUser) return sendError(res, "Email already registered!")
 
   const newUser = new User({ name, email, password })
   await newUser.save()
-  function generateOTP() {
-    // Declare a digits variable
-    // which stores all digits
-    var digits = "0123456789"
-    let OTP = ""
-    for (let i = 0; i < 6; i++) {
-      OTP += digits[Math.floor(Math.random() * 10)]
-    }
-    return OTP
-  }
 
   let OTP = generateOTP()
 
@@ -32,14 +23,7 @@ exports.create = async (req, res) => {
   })
 
   await newEmailVerificationToken.save()
-  var transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "d850847dd66891",
-      pass: "5f4b4277e9afac",
-    },
-  })
+  var transport = generateMailTransporter()
   transport.sendMail({
     from: "moviscape@server.com",
     to: newUser.email,
@@ -59,13 +43,13 @@ exports.verifyEmail = async (req, res) => {
     return res.status(401).json({ error: "Invalid user" })
 
   const user = await User.findById(userId)
-  if (!user) return res.status(401).json({ error: "user not found" })
+  if (!user) return res.status(404).json({ error: "user not found" })
 
   if (user.isVerified)
     return res.status(401).json({ error: "user is already verified" })
 
   const token = await EmailVerificationToken.findOne({ owner: userId })
-  if (!token) return res.status(401).json({ error: "token not found" })
+  if (!token) return res.status(404).json({ error: "token not found" })
 
   const isMatched = await token.compareToken(OTP)
   if (!isMatched)
@@ -75,14 +59,7 @@ exports.verifyEmail = async (req, res) => {
   await user.save()
 
   await EmailVerificationToken.findByIdAndDelete(token._id)
-  var transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "d850847dd66891",
-      pass: "5f4b4277e9afac",
-    },
-  })
+  var transport = generateMailTransporter()
   transport.sendMail({
     from: "moviscape@server.com",
     to: user.email,
@@ -98,7 +75,7 @@ exports.resendEmailVerificationToken = async (req, res) => {
   const { userId } = req.body
 
   const user = await User.findById(userId)
-  if (!user) return res.status(401).json({ error: "user not found" })
+  if (!user) return res.status(404).json({ error: "user not found" })
   if (user.isVerified)
     return res.status(401).json({ error: "Email is already verified" })
 
@@ -107,16 +84,7 @@ exports.resendEmailVerificationToken = async (req, res) => {
     return res.status(401).json({
       error: "Only one OTP can be requested within an hour.Please wait!",
     })
-  function generateOTP() {
-    // Declare a digits variable
-    // which stores all digits
-    var digits = "0123456789"
-    let OTP = ""
-    for (let i = 0; i < 6; i++) {
-      OTP += digits[Math.floor(Math.random() * 10)]
-    }
-    return OTP
-  }
+
   let OTP = generateOTP()
 
   const newEmailVerificationToken = new EmailVerificationToken({
@@ -125,14 +93,7 @@ exports.resendEmailVerificationToken = async (req, res) => {
   })
 
   await newEmailVerificationToken.save()
-  var transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "d850847dd66891",
-      pass: "5f4b4277e9afac",
-    },
-  })
+  var transport = generateMailTransporter()
   transport.sendMail({
     from: "moviscape@server.com",
     to: user.email,
---
+++ backend/utils/mail.js
@@ -0,0 +1,19 @@
+exports.generateOTP = (length = 6) => {
+  // Declare a digits variable
+  // which stores all digits
+  var digits = "0123456789"
+  let OTP = ""
+  for (let i = 0; i < length; i++) {
+    OTP += digits[Math.floor(Math.random() * 10)]
+  }
+  return OTP
+}
+exports.generateMailTransporter = () =>
+  nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "d850847dd66891",
+      pass: "5f4b4277e9afac",
+    },
+  })
