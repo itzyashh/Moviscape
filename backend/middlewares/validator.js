0ca69896a9e4084c82a0996855bd7f650ecd3fd6
itzyashh <yashjadhav1411@rocketmail.com>
2022-11-30 01:15:01 +0530

    fix: email bypass

--- backend/middlewares/validator.js
+++ backend/middlewares/validator.js
@@ -17,5 +17,8 @@ exports.userValidator = [
 
 exports.validate = (req, res, next) => {
   const error = validationResult(req).array()
+  if (error.length) {
+    return res.status(401).json({ error: error[0].msg })
+  }
   next()
 }
